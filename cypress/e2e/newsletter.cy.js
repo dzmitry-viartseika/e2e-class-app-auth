766a718383a92bba6b473e4e3951f807cbcc14e8
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-03-28 14:34:03 +0200

    feat: implement sign up/login e2e tests
    - implement e2e tests

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -10,24 +10,28 @@ describe('Newsletter', () => {
     cy.wait('@subscribe');
     cy.contains('Thanks for signing up');
   });
+
   it('should display validation errors', () => {
+    const RESPONSE_MESSAGE = 'Email exists already';
     cy.intercept('POST', '/newsletter*', {
-      message: 'Email exists already.',
-    }).as('subscribe'); // intercept any HTTP request localhost:3000/newsletter?anything
+      message: RESPONSE_MESSAGE,
+    }).as('subscribe');
     cy.visit('/');
     cy.get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.wait('@subscribe');
-    cy.contains('Email exists already.');
-  });
+    cy.contains(RESPONSE_MESSAGE);
+  })
   it('should successfully create a new contact', () => {
+    const TEST_EMAIL_ADDRESS = 'test@example.com';
+    const CODE_STATUS_RESPONSE = 201;
     cy.request({
       method: 'POST',
       url: '/newsletter',
-      body: { email: 'test@example.com' },
+      body: { email: TEST_EMAIL_ADDRESS },
       form: true
     }).then(res => {
-      expect(res.status).to.eq(201);
+      expect(res.body.message).to.eq(CODE_STATUS_RESPONSE);
     });
   })
 });
